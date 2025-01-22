faa92680c08c3068844566ba0ae5bb26c78017c7
413f10r <ale-fioretti@live.it>
2025-01-22 16:53:34 +0100

    try-1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/**/
+
+const numbList = document.getElementById("numbers-list")
+
+function numRandom(min, max) {
+    Math.floor(Math.randomm() * max) + min;
+}
+for (let i = 0; i <= 5; i++) {
+    let numeroRandomico = numRandom(1, 50)
+}
+
+if (!(numArray.includes(numeroRandomico))) {
+    numArray.push(numRandomico)
+}
+
+
+
+
+
+const countdown = document.getElementById("contdown")
+
+const timer = setInterval(function () ){
+    countdown--;
+}
+
+
+//controllo array
+
+for (
+    
+)
